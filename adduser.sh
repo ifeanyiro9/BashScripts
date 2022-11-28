358008c2984ef2f6caed96b6aeea73e9dcb850aa
Ifeanyi Otuonye <ifeanyiro@gmail.com>
2022-11-28 21:30:02 +0000

    Streamlined Script to create users using Arguements

--- adduser.sh
+++ adduser.sh
@@ -2,12 +2,8 @@
 
 #This script to accept user name(s) as arguments to add new users to server. If one of the user names already exist, it will exit the script, but if it doesnt, it will succefully create the user.
 
-totalnames=$#
-
-echo $totalnames
-
-#Read in input and save to username variable
-for i in {1..$totalnames} 
+#For loop to iterate through arguments
+for i in $*
 do
 
 	#Check if username is in passwd database and send all other unimportant information to /dev/null
@@ -20,14 +16,11 @@ do
 			#Add user if return value is not equal to 0
   			then
 				sudo useradd -m $i;
-				echo "$i user has been added!"      
-	
-        			echo "Enter password for $i"      
-				sudo passwd $i;
+				echo "$i user has been added!" 
 
 			#Notify that user already exist and exit script
   			else
-	 			echo "Unsuccessful. $username already exists."
+	 			echo "Unsuccessful. $i already exists."
 
 		fi
 done
