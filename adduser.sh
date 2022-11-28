494e00dcc950a818558f1a3eed8ef2d822c3b309
Ifeanyi Otuonye <ifeanyiro@gmail.com>
2022-11-28 20:32:28 +0000

    First Try to Complex Git/GitHub Project

--- adduser.sh
+++ adduser.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-#This script will prompt a user to enter new user name to add to server. If the user name already exist, it will exit the script, but if it doesnt, it will succefully create the user.
+#This script to accept user name(s) as arguments to add new users to server. If one of the user names already exist, it will exit the script, but if it doesnt, it will succefully create the user.
 
-#Prompt user to type user name to add
-echo "Please enter a new user name to add to server"
+totalnames=$#
+
+echo $totalnames
 
 #Read in input and save to username variable
-read username
+for i in {1..$totalnames} 
+do
 
-#Check if username is in passwd database and send all other unimportant information to /dev/null
-getent passwd $username  > /dev/null
+	#Check if username is in passwd database and send all other unimportant information to /dev/null
+	getent passwd $i  > /dev/null
 
-#Use $? to find return value of last run command. If username was found, it will return 0. Therefore we can check if $username exist by accessing the condition through if statement. 
- 
-if [ $? -ne 0 ]; 
+	#Use $? to find return value of last command. If username was found, it will return 0. We can check if $username exist by the condition if statement. 
+	 
+		if [ $? -ne 0 ]; 
 
-#Add user if return value is not equal to 0
-  then
-	sudo useradd -m $username;
-	echo "$username user has been added!"      
+			#Add user if return value is not equal to 0
+  			then
+				sudo useradd -m $i;
+				echo "$i user has been added!"      
 	
-        echo "Enter password for $username"      
-	sudo passwd $username;
+        			echo "Enter password for $i"      
+				sudo passwd $i;
 
-#Notify that user already exist and exit script
-  else
-	 echo "Unsuccessful. $username already exists."
+			#Notify that user already exist and exit script
+  			else
+	 			echo "Unsuccessful. $username already exists."
 
-fi
+		fi
+done
